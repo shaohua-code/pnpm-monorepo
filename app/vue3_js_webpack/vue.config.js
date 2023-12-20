1526deb30cfb85ff786e7443bf998187d9e02790
刘少华 <1377250790@qq.com>
2023-12-20 15:02:10 +0800

    ss

--- app/vue3_js_webpack/vue.config.js
+++ app/vue3_js_webpack/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  // lintOnSave: false,关闭esline
+  // lintOnSave:   false,关闭esline
   transpileDependencies: true,
 });
